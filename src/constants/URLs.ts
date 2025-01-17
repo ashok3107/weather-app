7b0c4fc6cfd92815469760e97e5cc6487665b125
Ashok R <ashokmaddy1996@gmail.com>
2025-01-17 00:16:51 -0600

    changed temp to Farenheit

--- src/constants/URLs.ts
+++ src/constants/URLs.ts
@@ -1,7 +1,7 @@
 const OPEN_WEATHER_APP_ID = 'b986c94f16c52b4260a7575d3433d1fd';
 const URLs = {
-  GET_WEATHER_DETAILS_BY_LOCATION: `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${OPEN_WEATHER_APP_ID}`,
-  GET_WEATHER_DETAILS_BY_CITY_ID: `https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid=${OPEN_WEATHER_APP_ID}`,
+  GET_WEATHER_DETAILS_BY_LOCATION: `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${OPEN_WEATHER_APP_ID}&units=imperial`,
+  GET_WEATHER_DETAILS_BY_CITY_ID: `https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid=${OPEN_WEATHER_APP_ID}&units=imperial`,
 };
 
 export default URLs;
